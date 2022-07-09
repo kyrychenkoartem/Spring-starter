1d4be4ae3b9ba2dcab4c9b1e3ecbdc84c6be9bad
kyrychenkoartem <killalik2009@gmail.com>
2022-07-09 15:56:07 +0200

    Implemented Annotation-based Configuration

--- src/main/java/com/artem/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/artem/spring/database/repository/CompanyRepository.java
@@ -1,25 +1,42 @@
 package com.artem.spring.database.repository;
 
 import com.artem.spring.bpp.Auditing;
-import com.artem.spring.bpp.InjectBean;
 import com.artem.spring.bpp.Transaction;
 import com.artem.spring.database.entity.Company;
 import com.artem.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company>{
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+    private final ConnectionPool pool1;
+
+    private final List<ConnectionPool> pools;
+
+    private final Integer poolSize;
+
+    public CompanyRepository(ConnectionPool pool1,
+                             List<ConnectionPool> pools,
+                             @Value("${db.pool.size}") Integer poolSize) {
+        this.pool1 = pool1;
+        this.pools = pools;
+        this.poolSize = poolSize;
+    }
 
     @PostConstruct
     private void init(){
         System.out.println("init company repository");
     }
+
     @Override
     public Optional<Company> findById(Integer id) {
         System.out.println("findById method..");
@@ -30,4 +47,5 @@ public class CompanyRepository implements CrudRepository<Integer, Company>{
     public void delete(Company entity) {
         System.out.println("delete method...");
     }
+
 }
